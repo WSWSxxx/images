6f21059ce6faf9326fdb3c25b1092949ea25b6af
WSWSxxx <109327321+WSWSxxx@users.noreply.github.com>
2022-07-17 14:12:27 +0800

    Upload by PicGo

---
+++ images/Department.java
@@ -0,0 +1,14 @@
+package com.pb.demo;
+
+public class Department {
+    private String dname;
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+}
